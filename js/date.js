5f06c4cfd19a630d6306dd4512da2453413ee6d5
Damileee <olasojudamilare6@gmail.com>
2025-07-20 03:55:45 +0100

    Add date.js for dynamic year and last modified date display

---
+++ js/date.js
@@ -0,0 +1,5 @@
+// Dynamic Year
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Last Modified Date
+document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
